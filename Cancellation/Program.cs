837e7a88e8ea9c5458aeb3de9e0a01c6952c22fb
Daniel Lerps <d@niel.lerps.de>
2020-08-13 14:22:05 +0200

    CancellationToken Example

---
+++ Cancellation/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Cancellation.Services;
+
+namespace Cancellation
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var clock = new ClockService();
+            var cts = new CancellationTokenSource();
+            var timeout = new Random().Next(5000, 15000);
+            
+            // Starting the clock and cancel timer
+            var stopTask = CancelAfter(timeout, cts);
+            var clockTask = clock.WhatsTheTime(cts.Token);
+
+            // wait for everything to complete gracefully
+            await Task.WhenAll(new[] { clockTask, stopTask });
+        }
+
+        private static async Task CancelAfter(int timeout, CancellationTokenSource cts)
+        {
+            Console.WriteLine($"This clock will stop running in {timeout / 1000} sec");
+
+            await Task.Delay(timeout);
+
+            cts.Cancel();
+        }
+    }
+}
---
+++ Cancellation/Services/ClockService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Cancellation.Services
+{
+    public class ClockService
+    {
+        public async Task WhatsTheTime(CancellationToken cancellationToken)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                Console.WriteLine($"{DateTime.Now:hh:mm:ss}");
+                
+                // don't pass the token and use the loop condition to exit
+                //await Task.Delay(1000);
+
+                // pass the token into the delay method -> causes an exception on cancel which needs to be handled
+                try
+                {
+                    await Task.Delay(1000, cancellationToken);
+                }
+                catch (OperationCanceledException oce)
+                {
+                    Console.WriteLine($"Cancellation caused {oce.GetType().Name}. Exiting now!");
+                    return;
+                }
+            }
+            
+            Console.WriteLine("Cancellation requested... stopping now");
+        }
+    }
+}
